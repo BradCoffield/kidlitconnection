fe00c0fe1d31aa3ba80181c4d46f16b1b39cb817
Brad <bcoffield@gmail.com>
2020-09-05 09:17:07 -0600

    forty

---
+++ .eleventy.js
@@ -0,0 +1,33 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("images");
+    // eleventyConfig.setUseGitIgnore(false);
+    // eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.setTemplateFormats([
+      "md",
+      "css",
+      "html",
+      "jpg",
+      "doc",
+      "png",
+      "gif",
+      "ico",
+      "svg",
+      "otf",
+      "eot",
+      "ttf",
+      "woff",
+      "woff2"
+    ]);
+  
+    return {
+      dir: {
+        input: "./src",
+        output: "_site", // Equivalent to Jekyll's destination property
+      },
+      passthroughFileCopy: true,
+      includes: "_includes",
+      layouts: "_layouts",
+    };
+  };
+  
