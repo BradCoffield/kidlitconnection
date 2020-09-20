e218e52d0e71b06ac52935cabd759efd1eb44607
Brad <bcoffield@gmail.com>
2020-09-20 11:53:02 -0600

    made some changes for passthrough and processed some images

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
+const Image = require("@11ty/eleventy-img");
+
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPassthroughCopy("images");
-    // eleventyConfig.setUseGitIgnore(false);
-    // eleventyConfig.addPassthroughCopy("css");
-    eleventyConfig.setTemplateFormats([
+    eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy("src/images");
+       eleventyConfig.setTemplateFormats([
       "md",
       "css",
       "html",
@@ -20,15 +20,50 @@ module.exports = function (eleventyConfig) {
       "woff2",
       "njk"
     ]);
+    eleventyConfig.addNunjucksAsyncShortcode("Image", async (src, alt) => {
+      if (!alt) {
+        throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+      }
   
+      let stats = await Image(src, {
+        widths: [25, 320, 640, 960, 1200, 1800, 2400],
+        formats: ["jpeg", "webp"],
+        urlPath: "/images/",
+        outputDir: "./_site/images/",
+      });
+  
+      let lowestSrc = stats["jpeg"][0];
+  
+      const srcset = Object.keys(stats).reduce(
+        (acc, format) => ({
+          ...acc,
+          [format]: stats[format].reduce(
+            (_acc, curr) => `${_acc} ${curr.srcset} ,`,
+            ""
+          ),
+        }),
+        {}
+      );
+  
+      const source = `<source type="image/webp" srcset="${srcset["webp"]}" >`;
+  
+      const img = `<img
+        loading="lazy"
+        alt="${alt}"
+        src="${lowestSrc.url}"
+        sizes='(min-width: 1024px) 1024px, 100vw'
+        srcset="${srcset["jpeg"]}"
+        width="${lowestSrc.width}"
+        height="${lowestSrc.height}">`;
+  
+      return `<div class="image-wrapper"><picture> ${source} ${img} </picture></div>`;
+    });
     return {
       dir: {
         input: "./src",
         output: "_site", // Equivalent to Jekyll's destination property
       },
-      passthroughFileCopy: true,
-      includes: "_includes",
-       
+ 
     };
   };
   
